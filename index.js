db9af4351deec2742698954a669f467921d4d531
Trillionclues <exceln646@gmail.com>
2022-06-28 20:01:09 +0100

    add pagination query

--- index.js
+++ index.js
@@ -6,12 +6,15 @@ const queryUrl = baseUrl + '/discover/movie?sort_by=popularity.desc&' + apiKey
 const imgUrl = 'https://image.tmdb.org/t/p/w500'
 const searchUrl = baseUrl + '/search/movie?' + apiKey
 
-console.log(queryUrl)
+// console.log(queryUrl)
 
 // define DOM elements
-const form = document.getElementById('form')
+// const form = document.getElementById('form')
+const form = document.querySelector('.form')
 const main = document.getElementById('movie-list')
 const search = document.getElementById('search')
+
+console.log(form)
 
 // page navigation elements
 const prevMovie = document.getElementById('prev')
@@ -33,7 +36,7 @@ function fetchMovies(url) {
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
-      console.log(data.results)
+      // console.log(data.results)
 
       if (data.results.length !== 0) {
         displayMovies(data.results)
@@ -43,6 +46,20 @@ function fetchMovies(url) {
         totalPages = data.total_pages
 
         currentMovie.innerHTML = currentPage
+
+        // disable current page
+        if (currentPage <= 1) {
+          prevMovie.classList.add('disabled')
+          nextMovie.classList.remove('disabled')
+        } else if (currentPage >= totalPages) {
+          prevMovie.classList.remove('disabled')
+          nextMovie.classList.add('disabled')
+        } else {
+          prevMovie.classList.remove('disabled')
+          nextMovie.classList.remove('disabled')
+        }
+      } else {
+        main.innerHTML = `<h1 class="no-results">No Results Found</h1>`
       }
     })
 }
@@ -57,7 +74,7 @@ function displayMovies(data) {
     movieDetail.classList.add('movie')
     movieDetail.innerHTML = `
    <img src="${
-     poster_path ? imgUrl + poster_path : 'http://via.placeholder.com/1080x1580'
+     poster_path ? imgUrl + poster_path : 'http://via.placeholder.com/300x400'
    }" alt="${title}">
 
    <div class="movie-info">
@@ -70,7 +87,7 @@ function displayMovies(data) {
     <h3>Overview</h3>
     ${overview}
     </br>
-        <button class"movie_modal" id="${id}">Watch Trailer</button>
+        <button class"movie_modal" id="${id}"><i class="fa-brands fa-youtube"></i>Watch Trailer</button>
    </div>
   
    `
@@ -93,3 +110,48 @@ function get_vote_count(vote) {
     return 'red'
   }
 }
+
+// page navigation
+prevMovie.addEventListener('click', () => {
+  if (prevPage > 0) {
+    pageCall(prevPage)
+  }
+})
+
+nextMovie.addEventListener('click', () => {
+  if (nextPage <= totalPages) {
+    pageCall(nextPage)
+  }
+})
+
+// page call callback
+function pageCall(page) {
+  let urlSwitch = finalUrl.split('?')
+  let querySwitch = urlSwitch[1].split('&')
+  let keyUrl = querySwitch[querySwitch.length - 1].split('=')
+
+  if (keyUrl[0] != 'page') {
+    let newUrl = finalUrl + '&page=' + page
+    fetchMovies(newUrl)
+  } else {
+    keyUrl[1] = page.toString()
+    let x = keyUrl.join('=')
+    querySwitch[querySwitch.length - 1] = x
+    let y = querySwitch.join('&')
+    let newUrl = urlSwitch[0] + '?' + y
+    fetchMovies(newUrl)
+  }
+}
+
+// form Queries
+// form.addEventListener('submit', (e) => {
+//   e.preventDefault()
+
+//   // console.log(e)
+//   const searchQuery = search.value
+//   if (searchQuery) {
+//     fetchMovies(searchUrl + '&query=' + searchQuery)
+//   } else {
+//     fetchMovies(queryUrl)
+//   }
+// })
